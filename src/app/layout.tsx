579a1bd6b324e749f38a20418bc7e1a37e77130a
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2024-10-10 18:45:06 +0530

    add: description & icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Issue Scout | GSOC Issue Tracker",
+  description: "GSoC Issue Tracker is a simple, open-source tool for managing project tasks, tracking issues, supporting collaboration, and real-time updates for open-source contributions and development.",
+  icons: {
+    icon: "/logo/logo-wbg.png"
+  }
 };
 
 export default function RootLayout({
