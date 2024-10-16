0de243a8fce934205bf78ffee16e051d7f396083
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2024-10-16 21:23:32 +0530

    add: starter code of express backend

---
+++ apps/tracker-backend/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
